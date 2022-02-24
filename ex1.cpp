3a1aed80b6fb104b5a8c8be874d3bc3d77cbc847
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-02-24 18:44:36 +0800

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+printf("I am %d years old \n",16 );
+printf("My birthday is%d  \n",950503 );
+printf("My height is%d cm \n" ,171 );
+printf("My weight is%d kg \n" ,75 );
+//printf("" );
+    cout<<"Hello World";
+
+    return 0;
+}
